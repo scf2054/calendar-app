586a7b21bc50b6103fc8496d1d4eb2c4a1cbf94e
Sam Frost <frosts1125@gmail.com>
2021-12-18 16:15:54 -0500

    removed the space separating the commas

--- server/api/api_utils/post_utils.py
+++ server/api/api_utils/post_utils.py
@@ -5,5 +5,5 @@ def default_user_calendar(id):
     # Get all of the special events' id's
     defaults = exec_get_all(f"SELECT * FROM {EVENT_TABLE} WHERE id < 5;")
     components = f"({U_ID}, {SUNDAY}, {MONDAY}, {TUESDAY}, {WEDNESDAY}, {THURSDAY}, {FRIDAY}, {SATURDAY})"
-    values = f"({id}, '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4')"
+    values = f"({id}, '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4')"
     exec_commit(f"INSERT INTO {CALENDAR_TABLE}{components} VALUES {values};")
