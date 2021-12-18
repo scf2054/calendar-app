e1ef56ecfa1c9bdae3f34836e035dbfc5b267a05
Sam Frost <frosts1125@gmail.com>
2021-12-18 16:42:37 -0500

    Created a put for a single user

--- server/api/user_api.py
+++ server/api/user_api.py
@@ -9,11 +9,15 @@ from .constants import *
 from .api_utils.post_utils import default_user_calendar
 
 HELP_USERNAME = "Your username for the account."
-HELP_USER_TYPE  = "Can be one of four: school, work, hybrid and other."
+HELP_USER_TYPE  = "The user type can be one of four: school, work, hybrid and other."
 
 users_post_args = reqparse.RequestParser()
 users_post_args.add_argument(USERNAME, type=str, help=HELP_USERNAME, required=True)
 users_post_args.add_argument(USER_TYPE, type=str, help=HELP_USER_TYPE, required=False)
+
+user_put_args = reqparse.RequestParser()
+user_put_args.add_argument(USERNAME, type=str, help=HELP_USERNAME, required=False)
+user_put_args.add_argument(USER_TYPE, type=str, help=HELP_USER_TYPE, required=False)
 
 class Users(Resource):
     def get(self):
@@ -35,4 +39,16 @@ class User(Resource):
     def get(self, id):
         return exec_get_all(f"SELECT * FROM user_table WHERE id = {id};")
 
-    
+    def put(self, id):
+        args = user_put_args.parse_args()
+        username = args[USERNAME]
+        user_type = args[USER_TYPE]
+        if len(exec_get_all(f"SELECT * FROM {USER_TABLE} WHERE {USERNAME} = '{username}';")) == 0:
+            if username != None:
+                exec_commit(f"UPDATE {USER_TABLE} SET {USERNAME} = '{username}' WHERE {ID} = {id};")
+            if user_type != None:
+                if user_type != 'school' and user_type != 'work' and user_type != 'hybrid' and user_type != 'other':
+                    return f"The user type '{user_type}' does not exist. {HELP_USER_TYPE}", 404
+                exec_commit(f"UPDATE {USER_TABLE} SET {USER_TYPE} = '{user_type}' WHERE {ID} = {id};")
+        else:  
+            return f"The username '{username}' already exists!", 403
