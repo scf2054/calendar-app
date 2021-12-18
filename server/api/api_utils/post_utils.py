f40d17f0b171cd3a18f3e8218b75a8fb3c2a1056
Sam Frost <frosts1125@gmail.com>
2021-12-18 15:45:38 -0500

    Created default_user_calendar before testing

--- server/api/api_utils/post_utils.py
+++ server/api/api_utils/post_utils.py
@@ -1,2 +1,9 @@
-def default_user(id):
-    return id
+from ..db.db_utils import *
+from ..constants import *
+
+def default_user_calendar(id):
+    # Get all of the special events' id's
+    defaults = exec_get_all(f"SELECT * FROM {EVENT_TABLE} WHERE id < 5;")
+    components = f"({U_ID}, {SUNDAY}, {MONDAY}, {TUESDAY}, {WEDNESDAY}, {THURSDAY}, {FRIDAY}, {SATURDAY})"
+    values = f"({id}, '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4', '1, 2, 3, 4')"
+    exec_commit(f"INSERT INTO {CALENDAR_TABLE}{components} VALUES {values};")
