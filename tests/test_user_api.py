a5455d939a86391a0197693103fdd3e9e1cc795f
Sam Frost <frosts1125@gmail.com>
2021-12-18 16:42:45 -0500

    Added tests for the put

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -25,3 +25,34 @@ class TestUser(unittest.TestCase):
         post_rest_call(self, 'http://127.0.0.1:5000/users', {USERNAME: 'Jill Conti', USER_TYPE: 'school'})
         self.assertEqual((2, 2, '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4', '1,2,3,4'), exec_get_one(f"SELECT * FROM {CALENDAR_TABLE} WHERE {ID} = 2;"), "New calendar not inserted")
         print("New account created!")
+
+    def test_put_users_fail_username(self):
+        try:
+            put_rest_call(self, 'http://127.0.0.1:5000/users/1', {USERNAME: 'Sam Frost', USER_TYPE: 'school'})
+            self.assertTrue(False, "Post username did not fail")
+        except:
+            self.assertTrue(True)
+            print("Post username has failed!")
+
+    def test_put_users_fail_user_type(self):
+        try:
+            put_rest_call(self, 'http://127.0.0.1:5000/users/1', {USERNAME: 'Jill Conti', USER_TYPE: 'sschool'})
+            self.assertTrue(False, "Post user type did not fail")
+        except:
+            self.assertTrue(True)
+            print("Post user type has failed!")
+
+    def test_put_users_username(self):
+        put_rest_call(self, 'http://127.0.0.1:5000/users/1', {USERNAME: 'Jill Conti'})
+        self.assertEqual((1, 'Jill Conti', 'hybrid'), exec_get_one(f"SELECT * FROM {USER_TABLE} WHERE {ID} = 1;"), "Username was not changed")
+        print("Username was changed!")
+
+    def test_put_users_user_type(self):
+        put_rest_call(self, 'http://127.0.0.1:5000/users/1', {USER_TYPE: 'school'})
+        self.assertEqual((1, 'Sam Frost', 'school'), exec_get_one(f"SELECT * FROM {USER_TABLE} WHERE {ID} = 1;"), "User type was not changed")
+        print("User type was changed!")
+
+    def test_put_users_both(self):
+        put_rest_call(self, 'http://127.0.0.1:5000/users/1', {USERNAME: 'Jill Conti', USER_TYPE: 'school'})
+        self.assertEqual((1, 'Jill Conti', 'school'), exec_get_one(f"SELECT * FROM {USER_TABLE} WHERE {ID} = 1;"), "User type was not changed")
+        print("Username and user type was changed!")
