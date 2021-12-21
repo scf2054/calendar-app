068609cde0d2b38ab89608871f394a361d5a5d95
Sam Frost <frosts1125@gmail.com>
2021-12-21 14:30:36 -0500

    Deleted beefy functions

--- server/api/api_utils/post_utils.py
+++ server/api/api_utils/post_utils.py
@@ -69,6 +69,7 @@ def optimize_medium_priorities(medium_priorities, free_time):
         # Get the starting and ending time
         event_start = event[4]
         event_end = event[5]
+        optimized = False
         # For each time frame in free time...
         for free_start in free_time:
             free_end = free_time[free_start]
@@ -87,7 +88,17 @@ def optimize_medium_priorities(medium_priorities, free_time):
                 new_end = free_end
                 # The new start of the event is the new end minus the length
                 new_start = subtract_times(new_end, event_length)
-                # If the length of the free time is equal to that of the event length...
+                optimized = True
+            # If the event overlaps to the right of a free time and the length of the free time is greater than or equal to that of the event...
+            elif overlaps_right(event_start, event_end, free_start, free_end) and time_is_greater(free_length, event_length, True):
+                # The new start of the event is the start of free time
+                new_start = free_start
+                # The new end of the event is the new start plus the length of the evnt
+                new_end = add_times(new_start, event_length)
+                optimized = True
+
+            if optimized:
+            # If the length of the free time is equal to that of the event length...
                 if event_length == free_length:
                     # Remove this free time from the dictionary entirely
                     free_time.pop(free_start)
@@ -95,29 +106,6 @@ def optimize_medium_priorities(medium_priorities, free_time):
                 else:
                     # Shorten the free time
                     free_time[free_start] = new_start
-                break
-            # If the event overlaps to the right of a free time and the length of the free time is greater than or equal to that of the event...
-            elif overlaps_right(event_start, event_end, free_start, free_end) and time_is_greater(free_length, event_length, True):
-                # The new start of the event is the start of free time
-                new_start = free_start
-                # The new end of the event is the new start plus the length of the evnt
-                new_end = add_times(new_start, event_length)
-
-def optimize_priorities(all_events, priorities, free_time, day_id, cal_id):
-    for priority in priorities:
-        event_start = priority[4]
-        event_end = priority[5]
-        event_length = length_between_times(event_end, event_start)
-        is_during_event = in_between_events(free_time, priority, event_length)
-        if is_during_event[0]:
-            update_times(is_during_event[1], priority, all_events, event_length, free_time, day_id, cal_id)
-            continue
-        for free_start in free_time:
-            free_end = free_time[free_start]
-            free_length = length_between_times(free_end, free_start)
-            free_greater_than_event = time_is_greater(free_length, event_length, True)
-            if free_greater_than_event:
-                update_times(free_start, priority, all_events, event_length, free_time, day_id, cal_id)
                 break
 
 def overlaps_left(this_start, this_end, that_start, that_end):
@@ -131,25 +119,6 @@ def overlaps_middle(this_start, this_end, that_start, that_end):
 
 def overlaps_at_all(this_start, this_end, that_start, that_end):
     return overlaps_left(this_start, this_end, that_start, that_end) or overlaps_right(this_start, this_end, that_start, that_end) or overlaps_middle(this_start, this_end, that_start, that_end)
-
-def update_times(new_start, event, all_events, event_length, time_frames, day_id, cal_id):
-    new_end = add_times(new_start, event_length)
-    time_frames[new_end] = time_frames[new_start]
-    time_frames.pop(new_start)
-    if event[2] == 'special':
-        special_event = exec_get_one(f"SELECT * FROM {EVENT_TABLE} WHERE {EVENT_NAME} = '{event[1]}' AND {EVENT_TYPE} = 'special' AND {EVENT_PRIORITY} = 2 AND {START_TIME} = '{new_start}' AND {END_TIME} = '{new_end}';")
-        if not special_event:
-            exec_commit(f"INSERT INTO {EVENT_TABLE}({EVENT_NAME}, {EVENT_TYPE}, {EVENT_PRIORITY}, {START_TIME}, {END_TIME}) VALUES ('{event[1]}', 'special', 2, '{new_start}', '{new_end}');")
-            special_event = exec_get_one(f"SELECT * FROM {EVENT_TABLE} WHERE {EVENT_NAME} = '{event[1]}' AND {EVENT_TYPE} = 'special' AND {EVENT_PRIORITY} = 2 AND {START_TIME} = '{new_start}' AND {END_TIME} = '{new_end}';")
-        new_events = ""
-        for event_id in all_events:
-            the_event = exec_get_one(f"SELECT * FROM {EVENT_TABLE} WHERE {ID} = {event_id};")
-            if the_event[1] != special_event[1]:
-                new_events += event_id + ","
-        new_events += str(special_event[0])
-        exec_commit(f"UPDATE {CALENDAR_TABLE} SET {get_day_by_id(day_id)} = '{new_events}' WHERE id = {cal_id};")
-    else:
-        exec_commit(f"UPDATE {EVENT_TABLE} SET {START_TIME} = '{new_start}', {END_TIME} = '{new_end}' WHERE {ID} = {event[0]};")
 
 def add_times(start, length):
     new_hr = int(start.split(':')[0]) + int(length.split(':')[0])
@@ -180,49 +149,6 @@ def get_total_minutes(e):
         hr += -2*hr
     min = int(e_split[1])
     return (hr*60) + min 
-
-def in_between_events(time_frames, event, event_length):
-    starts = list(time_frames.keys())
-    ends = list(time_frames.values())
-    starts.sort(key=get_total_minutes)
-    ends.sort(key=get_total_minutes)
-    event_start = event[4]
-    event_end = event[5]
-    i = 0
-    while i < len(starts):
-        j = i-1
-        that_start = ends[j]
-        that_end = starts[i]
-        if i == 0:
-            that_start = '0:00'
-        in_between = overlaps_middle(event_start, event_end, that_start, that_end)
-        if(in_between):
-            checks = 0
-            while checks < len(starts):
-                len_in_front = length_between_times(starts[i], ends[i])
-                len_in_back = length_between_times(starts[j], ends[j])
-                if event[2] == 'special' and event[1] != 'Sleep':
-                    if event[1] == 'Breakfast':
-                        meal_frame = BREAKFAST_FRAME
-                    elif event[1] == 'Lunch':
-                        meal_frame = LUNCH_FRAME
-                    elif event[1] == 'Dinner':
-                        meal_frame == DINNER_FRAME
-
-                    if time_is_greater(len_in_front, event_length) and overlaps_at_all(starts[i], ends[i], meal_frame[0], meal_frame[1]):
-                        return [True, starts[i]]
-                    elif time_is_greater(len_in_back, event_length) and overlaps_at_all(starts[j], ends[j], meal_frame[0], meal_frame[1]):
-                        return [True, starts[j]]
-                else:
-                    if time_is_greater(len_in_front, event_length):
-                        return [True, starts[i]]
-                    elif time_is_greater(len_in_back, event_length):
-                        return [True, starts[j]]
-                checks += 1
-                i += 1
-                j -= 1
-        i += 1
-    return [False]
 
 def length_between_times(this, that):
     this_split = this.split(':')
