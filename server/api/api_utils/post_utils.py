08ecb2f48e575be49b7e7e02ed17868db47c961b
Sam Frost <frosts1125@gmail.com>
2021-12-21 16:04:52 -0500

    Combined the algorithm for collecting highest priority events with the main function

--- server/api/api_utils/post_utils.py
+++ server/api/api_utils/post_utils.py
@@ -3,8 +3,8 @@ from ..constants import *
 import math
 
 BREAKFAST_FRAME = ['7:00', '12:00']
-LUNCH_FRAME = ['11:00', '4:00']
-DINNER_FRAME = ['3:00', '8:00']
+LUNCH_FRAME = ['11:00', '16:00']
+DINNER_FRAME = ['15:00', '20:00']
 
 def optimize_calendar(calendar):
     i = 2
@@ -13,9 +13,10 @@ def optimize_calendar(calendar):
         sleep = {}
         low_priorities = []
         medium_priorities = []
+        high_priorities = []
         free_time = {'0:00': '23:59'}
-        earliest = '23:59'
-        latest = '00:00'
+        # earliest = '23:59'
+        # latest = '00:00'
         events = calendar[i].split(",")
         # For every id in the calendar day...
         for id in events:
@@ -26,20 +27,22 @@ def optimize_calendar(calendar):
             priority = event[3]
             # If the priority is at its highest...
             if priority == 3:
-                # Fit this event into free time
-                for start in free_time:
-                    end = free_time[start]
-                    # If the event is in the middle of a certain free time frame...
-                    if overlaps_middle(event_start, event_end, start, end):
-                        # The original start of free time has an end that is the start of this event
-                        free_time[start] = event_start
-                        # A new start to free time is the end of this event, and its end is the original time frame's end
-                        free_time[event_end] = end
-                        if time_is_greater(earliest, event_start):
-                            earliest = event_start
-                        if time_is_greater(event_end, latest):
-                            latest = event_end
-                        break
+                # Add this to the high priority list
+                high_priorities.append(event)
+                # # Fit this event into free time
+                # for start in free_time:
+                #     end = free_time[start]
+                #     # If the event is in the middle of a certain free time frame...
+                #     if overlaps_middle(event_start, event_end, start, end):
+                #         # The original start of free time has an end that is the start of this event
+                #         free_time[start] = event_start
+                #         # A new start to free time is the end of this event, and its end is the original time frame's end
+                #         free_time[event_end] = end
+                #         if time_is_greater(earliest, event_start):
+                #             earliest = event_start
+                #         if time_is_greater(event_end, latest):
+                #             latest = event_end
+                #         break
             # If the event is a sleep event...
             elif event[1] == 'Sleep' and priority == 2:
                 # Initialize this in the sleep structure initialized before
@@ -51,21 +54,26 @@ def optimize_calendar(calendar):
             else:
                 # Otherwise, add it to the lowest priorities list
                 low_priorities.append(event)
+        # Optimize the highest priority group to get the earliest and latest times
+        earliest_latest = optimize_priority_group(high_priorities, free_time, 3)
         # Add the sleep schedule to free time...
         # Remove the value at '0:00' which should be stored in the 'earliest' variable already
         free_time.pop('0:00')
         bedtime = list(sleep.keys())[0]
         # The value at sleep's end should now be the earliest time
-        free_time[sleep[bedtime]] = earliest
+        free_time[sleep[bedtime]] = earliest_latest[0]
         # The value at the latest all events go should now be the bedtime
-        free_time[latest] = bedtime
-        optimize_priority_group(medium_priorities, free_time)
+        free_time[earliest_latest[1]] = bedtime
+        optimize_priority_group(medium_priorities, free_time, 2)
         i += 1
     print("finished")
 
-def optimize_priority_group(medium_priorities, free_time):
-    # For each medium priority...
-    for event in medium_priorities:
+def optimize_priority_group(priority_group, free_time, priority):
+    # Initialize the earliest and latest to return
+    earliest = '23:59'
+    latest = '00:00'
+    # For each event in priorities...
+    for event in priority_group:
         # Get the starting and ending time
         event_start = event[4]
         event_end = event[5]
@@ -78,29 +86,55 @@ def optimize_priority_group(medium_priorities, free_time):
             free_length = length_between_times(free_start, free_end)
             # As long as the length of free time is greater than or equal to the event...
             if time_is_greater(free_length, event_length, True):
+                is_during_event = during_event([event_start, event_end], [free_start, free_end], free_time)
                 # If the event takes place during free time...
+                # This should be the only "if" called for the highest priority
                 if overlaps_middle(event_start, event_end, free_start, free_end):
                     # Change the free time and leave the time frame alone
                     free_time[free_start] = event_start
                     free_time[event_end] = free_end
+                    # Only when the priority is 3 should you update the earliest and latest times
+                    if priority == 3:
+                        if time_is_greater(earliest, event_start):
+                            earliest = event_start
+                        if time_is_greater(event_end, latest):
+                            latest = event_end
                     break
+                # If the event overlaps an event that's already there...
+                elif is_during_event[0] and priority != 3:
+                    overlapped_event_end = is_during_event[1]
+                    # Calculate distance between right side and left side
+                    length_of_left_side = length_between_times(event_start, free_end)
+                    length_of_right_side = length_between_times(event_end, overlapped_event_end)
+                    # If distance is shorter on left side...
+                    if time_is_greater(length_of_right_side, length_of_left_side, True):
+                        # Set new start/end
+                        new_end = free_end
+                        new_start = subtract_times(new_end, event_length)
+                        optimized = True
+                    # If distance is shorter on right side and the free time frame length on the right side is greater than the event's...
+                    else:
+                        free_after_overlapped_end = free_time[overlapped_event_end]
+                        free_after_length = length_between_times(overlapped_event_end, free_after_overlapped_end)
+                        if time_is_greater(free_after_length, event_length, True):
+                            # Set new start/end
+                            new_start = overlapped_event_end
+                            new_end = add_times(new_start, event_length)
+                            optimized = True
                 # If the event overlaps to the left of a free time
-                if overlaps_left(event_start, event_end, free_start, free_end):
+                elif overlaps_left(event_start, event_end, free_start, free_end) and priority != 3:
                     # The new end of the event is the free time's end
                     new_end = free_end
                     # The new start of the event is the new end minus the length
                     new_start = subtract_times(new_end, event_length)
                     optimized = True
                 # If the event overlaps to the right of a free time
-                elif overlaps_right(event_start, event_end, free_start, free_end):
+                elif overlaps_right(event_start, event_end, free_start, free_end) and priority != 3:
                     # The new start of the event is the start of free time
                     new_start = free_start
                     # The new end of the event is the new start plus the length of the evnt
                     new_end = add_times(new_start, event_length)
                     optimized = True
-                # If the event overlaps an event that's already there...
-                elif during_event([event_start, event_end], [free_start, free_end], free_time):
-                    pass
                 if optimized:
                 # If the length of the free time is equal to that of the event length...
                     if event_length == free_length:
@@ -111,6 +145,8 @@ def optimize_priority_group(medium_priorities, free_time):
                         # Shorten the free time
                         free_time[free_start] = new_start
                     break
+    if priority == 3:
+        return [earliest, latest]
 
 def during_event(event_frame, free_frame, free_time):
     # Get the free time that is after this free time passed in
@@ -126,10 +162,10 @@ def during_event(event_frame, free_frame, free_time):
         overlapped_event_end = starts[index+1]
         # If the event is overlapping the middle of the time frame between the current end and the next start...
         # return True
-        return overlaps_middle_exactly(event_frame[0], event_frame[1], overlapped_event_start, overlapped_event_end)
-    except:
+        return [overlaps_middle(event_frame[0], event_frame[1], overlapped_event_start, overlapped_event_end), overlapped_event_end]
+    except IndexError:
         print("Event is during sleep schedule")
-        return False
+        return [False]
 
 def overlaps_left(this_start, this_end, that_start, that_end):
     return time_is_greater(this_start, that_start) and time_is_greater(this_end, that_end) and time_is_greater(that_end, this_start)
@@ -138,9 +174,6 @@ def overlaps_right(this_start, this_end, that_start, that_end):
     return time_is_greater(that_end, this_end) and time_is_greater(that_start, this_start) and time_is_greater(this_end, that_start)
 
 def overlaps_middle(this_start, this_end, that_start, that_end):
-    return time_is_greater(this_start, that_start) and time_is_greater(that_end, this_end)
-
-def overlaps_middle_exactly(this_start, this_end, that_start, that_end):
     return time_is_greater(this_start, that_start, True) and time_is_greater(that_end, this_end, True)
 
 def overlaps_at_all(this_start, this_end, that_start, that_end):
@@ -162,7 +195,7 @@ def subtract_times(end, length):
     new_min = int(end.split(':')[1]) - int(length.split(':')[1])
     if new_min < 0:
         new_hr -= math.floor(-new_min / 60) + 1
-        new_min = 60 + (new_min % 60)
+        new_min = 60 - (new_min % 60)
     if new_min < 10:
         new_min = f"0{new_min}"
     new_start = f"{new_hr}:{new_min}"
