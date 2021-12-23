533bfc13b52d0fd07c1538657607ddbef734fea5
Sam Frost <frosts1125@gmail.com>
2021-12-23 11:42:04 -0500

    Added more getters for different instances

--- server/api/event_api.py
+++ server/api/event_api.py
@@ -4,7 +4,16 @@ from flask_restful import request
 from flask_restful import reqparse
 import json
 from .db.db_utils import *
+from .constants import *
 
 class Events(Resource):
     def get(self):
-        return exec_get_all("SELECT * FROM event_table")
+        return exec_get_all(f"SELECT * FROM {EVENT_TABLE};")
+
+class Event(Resource):
+    def get(self, id):
+        return exec_get_one(f"SELECT * FROM {EVENT_TABLE} WHERE {ID} = {id};")
+
+class User_Events(Resource):
+    def get(self, u_id):
+        return exec_get_all(f"SELECT * FROM {EVENT_TABLE} WHERE {U_ID} = {u_id};")
