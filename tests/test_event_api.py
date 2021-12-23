4ec958b99f3950a73be23b3ba36831b8842583c5
Sam Frost <frosts1125@gmail.com>
2021-12-23 14:43:52 -0500

    Added tests for checking overlapping

--- tests/test_event_api.py
+++ tests/test_event_api.py
@@ -27,3 +27,36 @@ class TestEvent(unittest.TestCase):
         self.assertEqual(expected_hw, actual_hw, 'Homework event not added correctly')
         self.assertEqual(expected_class, actual_class, 'Class event was not added correctly')
         print("Homework and class events successfully added")
+
+    def test_post_user_events_wrong_event_type(self):
+        try:
+            post_rest_call(self, 'http://127.0.0.1:5000/events/user/1', {START_TIME: '8:00', DAY_ID: 1, EVENT_TYPE: 'failing event type', EVENT_PRIORITY: 3})
+            self.assertTrue(False, 'Event type was not caught')
+        except:
+            self.assertTrue(True)
+            print("Incorrect event type caught!")
+
+    def test_post_user_events_overlapping_events_middle(self):
+            try:
+                post_rest_call(self, 'http://127.0.0.1:5000/events/user/1', {START_TIME: '10:00', END_TIME: '10:50', DAY_ID: 2, EVENT_TYPE: 'work', EVENT_PRIORITY: 3})
+                self.assertTrue(False, "Event was not caught to be overlapping middle")
+            except Exception:
+                self.assertTrue(True, "Wrong error occurred for middle overlapping")
+                print("Overlapping middle was not added!")
+
+    def test_post_user_events_overlapping_events_left(self):
+        try:
+            post_rest_call(self, 'http://127.0.0.1:5000/events/user/1', {START_TIME: '11:30', END_TIME: '12:20', DAY_ID: 2, EVENT_TYPE: 'work', EVENT_PRIORITY: 3})
+            self.assertTrue(False, "Event was not caught to be overlapping left")
+        except Exception:
+            self.assertTrue(True, "Wrong error occurred for middle overlapping")
+            print("Overlapping left was not added!")
+
+
+    def test_post_user_events_overlapping_events_right(self):
+        try:
+            post_rest_call(self, 'http://127.0.0.1:5000/events/user/1', {START_TIME: '14:30', END_TIME: '15:20', DAY_ID: 2, EVENT_TYPE: 'work', EVENT_PRIORITY: 3})
+            self.assertTrue(False, "Event was not caught to be overlapping right")
+        except Exception:
+            self.assertTrue(True, "Wrong error occurred for middle overlapping")
+            print("Overlapping right was not added!")
