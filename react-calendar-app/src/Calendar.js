91d8d3b5732554a3c25606a3e5e508952fad551f
Sam Frost <frosts1125@gmail.com>
2021-12-31 11:37:42 -0500

    Initialized calendar view

--- react-calendar-app/src/Calendar.js
+++ react-calendar-app/src/Calendar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap'
+import { Row, Col, Table } from 'reactstrap'
 
 class Calendar extends Component {
     constructor(props) {
@@ -11,11 +11,107 @@ class Calendar extends Component {
 
     render() {
         return (
-            <Row>
-                <Col>
-                    {this.state.initial}
-                </Col>
-            </Row>
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope='row'>
+                            7:00am
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            8:00am
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            9:00am
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            10:00am
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            11:00am
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            12:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            1:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            2:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            3:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            4:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            5:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            6:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            7:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            8:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            9:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            10:00pm
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>
+                            11:00pm
+                        </th>
+                    </tr>
+                </tbody>
+            </Table>
         );
     }
 }
