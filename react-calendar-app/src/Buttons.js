be76c3c471a7ef9daa633ae759b49bf4728c6b07
Sam Frost <frosts1125@gmail.com>
2021-12-31 13:53:03 -0500

    Created buttons that toggle modal views

--- react-calendar-app/src/Buttons.js
+++ react-calendar-app/src/Buttons.js
@@ -1,19 +1,53 @@
 import './Buttons.css';
 
 import React, { Component } from 'react';
-import { Row, Col, Table, Container, Button } from 'reactstrap';
+import { Row, Col, Table, Container, Button, Modal } from 'reactstrap';
 
 class Buttons extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            initialize: true
+            view_edit_sleep: false,
+            view_create_event: false
         }
+    }
+
+    toggleEditSleep=(event)=> {
+        this.setState({view_edit_sleep: !this.state.view_edit_sleep});
+    }
+
+    toggleCreateEvent=(event)=> {
+        this.setState({view_create_event: !this.state.view_create_event});
     }
 
     render() {
         return (
-            <Button>Button</Button>
+            <>
+                <Col>
+                    <Button
+                        color='info'
+                        outline
+                        onClick={this.toggleEditSleep}
+                    >
+                        Edit Sleep Schedule
+                    </Button>
+                </Col>
+                <Col>
+                    <Button
+                        color='info'
+                        outline
+                        onClick={this.toggleCreateEvent}
+                    >
+                        Create Event
+                    </Button>
+                </Col>
+                <Modal className='edit-sleep-modal' isOpen={this.state.view_edit_sleep}>
+
+                </Modal>
+                <Modal className='create-event-modal' isOpen={this.state.view_create_event}>
+
+                </Modal>
+            </>
         );
     }
 }
